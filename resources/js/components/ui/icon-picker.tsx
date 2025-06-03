b47a2a7345d9296d00bfa298b4599ece898fed8b
Mitchell Flindell <mitch.flindell@gmail.com>
2025-06-03 15:56:14 +1000

    switch to path

--- resources/js/components/ui/icon-picker.tsx
+++ resources/js/components/ui/icon-picker.tsx
@@ -59,7 +59,7 @@ const IconPicker: React.FC<IconPickerProps> = ({ value, onChange, onClose, class
   };
 
   const handleImageUpload = (data: object) => {
-    onChange({ icon: undefined, emoji: undefined, url: data.url });
+    onChange({ icon: undefined, emoji: undefined, url: data.path });
     onClose?.();
   };
 
