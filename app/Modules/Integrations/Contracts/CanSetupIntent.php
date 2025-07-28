7fbf43a00e1b8945571ce512616be7dc9f2db7cf
Mitchell Flindell <mitch.flindell@gmail.com>
2025-07-29 06:55:03 +1000

    remove old setup intent

--- app/Modules/Integrations/Contracts/CanSetupIntent.php
+++ app/Modules/Integrations/Contracts/CanSetupIntent.php
@@ -6,7 +6,5 @@ use App\Models\Order\Order;
 
 interface CanSetupIntent
 {
-    public function createSetupIntent(Order $order, string $confirmationToken);
-
     public function getSetupIntent($intentId);
 }
